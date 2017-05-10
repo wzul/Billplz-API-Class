51f43072934ee37db7d6bdaea7d31e87c0d1c7ee
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2017-05-10 13:01:41 +0800

    Update CreateABill.php

--- CreateABill.php
+++ CreateABill.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file shows how to Create A Bill using BillplzCURL class
+ * This file shows how to Create A Bill using Billplz class
  * The commented line is Optional parameter
  * 
  */
@@ -9,26 +9,6 @@ require 'billplz.php';
 $api_key = '4e49de80-1670-4606-84f8-2f1d33a38670';
 
 $a = new Billplz;
-
-/*
- * Create Collection
- */
-$collectionid2 = $a->create_collection($api_key, 'Aikkkk');
-
-/*
- * Validate Collection
- */
-$booleanA = $a->check_collection_id($api_key, $collectionid2);
-
-/*
- * Get Collection Index at Page 1
- */
-$colIndex = $a->getCollectionIndex($api_key, '1');
-echo '<pre>'.print_r($colIndex, true).'</pre>';
-
-/*
- * Create Bills
- */
 $a->setName("Wan Zulkarnain");
 $a->setAmount('10.60');
 $a->setEmail('wan@wanzul-hosting.com');
@@ -45,21 +25,5 @@ $a->setPassbackURL('http://callback-url.com', 'http://redirect-url.com');
 //$a->setDeliver('3'); //Email & SMS Notification
 //$a->create_bill($api_key, true);
 $a->create_bill($api_key);
-echo $a->getURL();
-
-$billid = $a->getID();
-
-/*
- * Get Bills
- */
-$abc = $a->check_bill($api_key, $billid);
-echo '<pre>'.print_r($abc, true).'</pre>';
-
-/*
- * Delete Bills
- */
-if ($a->deleteBill($api_key, $billid)){
-    // Successfully deleted bills
-}else {
-    // Failed to delete bills
-}
+//echo $a->getURL();
+header('Location: ' .$a->getURL());
