4fdc033c252a59c2929e27e3a2d6b8b945ebd516
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2017-07-12 09:47:18 +0800

    Update billplz.php

--- billplz.php
+++ billplz.php
@@ -223,7 +223,7 @@ if (!class_exists('Billplz')) {
         }
 
         public function setAmount($amount) {
-            $this->array['amount'] = $amount * 100;
+            $this->array['amount'] = preg_replace("/[^0-9]/","",$amount) * 100;
             return $this;
         }
 
